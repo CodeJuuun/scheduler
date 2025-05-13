f6fcc28b97217b3c5c9133750f8b56e99bddbf25
CodeJuuun <Chomseng.a@gmail.com>
2025-05-13 15:50:05 -0400

    updated to match component to use pattern to render when mode component matches

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -11,22 +11,23 @@ const SHOW = "SHOW";
 
 
 export default function Appointment(props) {
-  const { mode, transition, back} = useVisualMode(
+  const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => console.log("CONFIRM")}
           onEdit={() => console.log("EDIT")}
+
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
     </article>
   );
 }
