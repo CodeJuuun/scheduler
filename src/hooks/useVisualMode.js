f8eabd4cd241cb2c98a21fbc8ae0e0bff5fa3208
CodeJuuun <Chomseng.a@gmail.com>
2025-05-13 15:16:22 -0400

    added transition function, back function, and test codes

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode) {
+    setHistory(prev => {
+     return [...prev, newMode];
+    })
+  }
+
+  function back() {
+    setHistory(prev => {
+      if (prev.length <= 1) {
+        return prev;
+      }
+      return [...prev.slice(0, prev.length - 1)];
+    })
+  }
+
+
+  return { mode: history[history.length - 1], transition, back };
+}
