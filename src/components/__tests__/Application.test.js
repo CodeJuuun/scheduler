958f69d8e71102d59cb0cccd09201ced5dbfbc1d
CodeJuuun <Chomseng.a@gmail.com>
2025-05-21 15:09:40 -0400

    corrected code, also accessed appropriate functions returned by render function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,21 +1,40 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import Application from "../Application"
+import '@testing-library/jest-dom';
+import { render, fireEvent, prettyDOM, findByText, getAllByTestId } from "@testing-library/react";
+import Application from "../Application";
 
 
-describe("Appointment", () => {
+describe("Application", () => {
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
-    const { findByText } = render(<Application />);
+    const { queryByText, findByText } = render(<Application />);
 
-    return findByText("Monday");
+    return findByText("Monday").then(() => {
+      fireEvent.click(queryByText("Tuesday"));
+      expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { queryByText, findByText } = render(<Application />);
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug, getAllByTestId, getByText, getByAltText, getByPlaceholderText } = render(<Application />);
 
-  return findByText("Monday").then(() => {
-    fireEvent.click(queryByText("Tuesday"));
-    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+    await findByText(container, "Archie Cohen");
+
+    const appointments = getAllByTestId(container, "appointment");
+
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
   });
-});
+
+
 });
